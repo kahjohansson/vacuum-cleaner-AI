e2fd90de3962336c83a46377b0019af55acbc561
Mayumi <kahjohansson@gmail.com>
2019-10-23 21:18:10 -0300

    Aplicando correções

--- AspiradorPo.R
+++ AspiradorPo.R
@@ -19,23 +19,20 @@ AspiradorPo <- function(desc = NULL, pai = NULL){
 ## Sobrecarregando o operador "==" para comparação entre estados
 Ops.AspiradorPo = function(obj1,obj2){
   if(.Generic == "=="){
-    ##objetivo <- AspiradorPo()
-    ##objetivo$desc <- c(NO = 0, NE = 0, SO = 0, SE = 0, AP = 0, C = 0)
-    ##if(obj1$desc[1] == objetivo$desc[1] && obj1$desc[2] == objetivo$desc[2] 
-    ##  && obj1$desc[3] == objetivo$desc[3] && obj1$desc[4] == objetivo$desc[4]){
-    ##  print("primeiro if")
-    ##  return (TRUE);
+
+    if(obj1$desc[1] == objetivo$desc[1] && obj1$desc[2] == objetivo$desc[2] 
+      && obj1$desc[3] == objetivo$desc[3] && obj1$desc[4] == objetivo$desc[4]){
+      return (TRUE);
     
-    ##}else if(obj2$desc[1] == objetivo$desc[1] && obj2$desc[2] == objetivo$desc[2] 
-    ##  && obj2$desc[3] == objetivo$desc[3] && obj2$desc[4] == objetivo$desc[4]){
-    ##  print("segundo if")
-    ##  return (TRUE);
+    }else if(obj2$desc[1] == objetivo$desc[1] && obj2$desc[2] == objetivo$desc[2] 
+      && obj2$desc[3] == objetivo$desc[3] && obj2$desc[4] == objetivo$desc[4]){
+      return (TRUE);
+  
+    }else{
+
+    return(obj1$desc[1] == obj2$desc[1] && obj1$desc[2] == obj2$desc[2] && obj1$desc[3] == obj2$desc[3] && obj1$desc[4] == obj2$desc[4])
     
-    ##}else{
-    ##  print("ultimo if")
-      return(obj1$desc[1] == obj2$desc[1] && obj1$desc[2] == obj2$desc[2] 
-      && obj1$desc[3] == obj2$desc[3] && obj1$desc[4] == obj2$desc[4])
-    ##}
+    }
   }
 }
 
@@ -77,28 +74,28 @@ geraFilhos.AspiradorPo <- function(obj) {
     if(desc[1] == 0){ ## quadrado do aspirador está limpo
       filhos <- list(c(desc[1],desc[2],desc[3],desc[4],2,1), c(desc[1],desc[2],desc[3],desc[4],3,3))
     
-    }else{ # quadrado do aspirador está sujo
+    }else if(desc[1] == 1){ # quadrado do aspirador está sujo
       filhos <- list(c(0,desc[2],desc[3],desc[4],desc[5],2), c(desc[1],desc[2],desc[3],desc[4],2,1), c(desc[1],desc[2],desc[3],desc[4],3,3))
     }
   }else if(desc[5] == 2){ ## aspirador está no canto superior direito
     if(desc[2] == 0){ ## quadrado do aspirador está limpo
       filhos <- list(c(desc[1],desc[2],desc[3],desc[4],4,3), c(desc[1],desc[2],desc[3],desc[4],1,1))
     
-    }else{ # quadrado do aspirador está sujo
+    }else{ if(desc[2] == 1) # quadrado do aspirador está sujo
       filhos <- list(c(desc[1],0,desc[3],desc[4],desc[5],2), c(desc[1],desc[2],desc[3],desc[4],4,3), c(desc[1],desc[2],desc[3],desc[4],1,1))
     }
   }else if(desc[5] ==  3){ ## aspirador está no canto inferior esquerdo
     if(desc[3] == 0){ ## quadrado do aspirador está limpo
       filhos <- list(c(desc[1],desc[2],desc[3],desc[4],4,1), c(desc[1],desc[2],desc[3],desc[4],1,3))
 
-    }else{ # quadrado do aspirador está sujo
+    }else if(desc[3] == 1){ # quadrado do aspirador está sujo
       filhos <- list(c(desc[1],desc[2],0,desc[4],desc[5],2), c(desc[1],desc[2],desc[3],desc[4],4,1), c(desc[1],desc[2],desc[3],desc[4],1,3))
     }
-  }else{ ## aspirador está no canto inferior direito
+  }else if(desc[5] ==  4){ ## aspirador está no canto inferior direito
     if(desc[4] == 0){ ## quadrado do aspirador está limpo
       filhos <- list(c(desc[1],desc[2],desc[3],desc[4],3,1), c(desc[1],desc[2],desc[3],desc[4],2,3))
 
-    }else{ # quadrado do aspirador está sujo
+    }else if(desc[4] == 1){ # quadrado do aspirador está sujo
       filhos <- list(c(desc[1],desc[2],desc[3],0,desc[5],2), c(desc[1],desc[2],desc[3],desc[4],3,1), c(desc[1],desc[2],desc[3],desc[4],2,3))
     }
   }
@@ -112,9 +109,10 @@ geraFilhos.AspiradorPo <- function(obj) {
     x$g <- obj$g + f[[6]]
     if(count == 1){
       filhosGerados<- x
+      count = 0
     }
-    filhosGerados <- c(filhosGerados, x)
+    filhosGerados <- c(filhosGerados, list(x))
   }
   
   return(filhosGerados)
-}
+} 
